3942da31b1fb893dac54cf635637fb8eebeb27c6
Valerant86 <144962202+Valerant86@users.noreply.github.com>
2023-12-04 02:12:08 -0600

    Update index.js

--- index.js
+++ index.js
@@ -19,30 +19,43 @@ function collectInputValues() {
     return inputValues;
 }
 
-// Function to update predictions when the button is clicked
-async function runExample() {
-    // Collect input values from text boxes
-    const inputValues = collectInputValues();
+// Async function to initialize the model and run example
+async function initializeAndRunExample() {
+    // ONNX model file name
+    const onnxModelFileName = 'xgboost_AirQuality_ort.onnx';
 
-    // Prepare the input data for ONNX model
-    const inputData = new Float32Array(Object.values(inputValues));
+    // Load the ONNX model
+    const onnxModel = await ort.InferenceSession.create({ backendHint: 'webgl' });
+    await onnxModel.loadModel(`./${onnxModelFileName}`);
 
-    // Create an ONNX Tensor from the input data
-    const inputTensor = new ort.Tensor(ort.WebGLFloat32, new Float32Array(inputData), [1, Object.keys(inputValues).length]);
+    // Extract input and output names
+    const inputName = onnxModel.inputNames[0];
+    const outputName = onnxModel.outputNames[0];
 
-    // Run the ONNX model to get predictions
-    const outputTensor = await onnxModel.run(['output'], { input: inputTensor });
+    // Function to update predictions when the button is clicked
+    window.runExample = async function () {
+        // Collect input values from text boxes
+        const inputValues = collectInputValues();
 
-    // Get the prediction result
-    const predictionResult = outputTensor.getValues();
+        // Prepare the input data for ONNX model
+        const inputData = new Float32Array(Object.values(inputValues));
 
-    // Display the prediction result
-    document.getElementById('predictions').textContent = `Predicted AQI: ${predictionResult[0].toFixed(2)}`;
+        // Create an ONNX Tensor from the input data
+        const inputTensor = new ort.Tensor(ort.WebGLFloat32, new Float32Array(inputData), [1, Object.keys(inputValues).length]);
+
+        // Run the ONNX model to get predictions
+        const outputTensor = await onnxModel.run([outputName], { [inputName]: inputTensor });
+
+        // Get the prediction result
+        const predictionResult = outputTensor.getValues();
+
+        // Display the prediction result
+        document.getElementById('predictions').textContent = `Predicted AQI: ${predictionResult[0].toFixed(2)}`;
+    };
+
+    // Update the model status on the HTML page
+    document.getElementById('modelLoaded').textContent = 'Yes';
 }
 
-// ONNX model file name
-const onnxModelFileName = 'xgboost_AirQuality_ort.onnx';
-
-// Load the ONNX model
-const onnxModel = await ort.InferenceSession.create({ backendHint: 'webgl' });
-await onnxModel.loadModel(`./${onnxModelFileName}`);
+// Call the initialization function
+initializeAndRunExample();
