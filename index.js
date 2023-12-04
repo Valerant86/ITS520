01deaf734cbb140b91ce1fdb2f30a77596566886
Valerant86 <144962202+Valerant86@users.noreply.github.com>
2023-12-04 02:51:17 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,55 +3,64 @@ async function initializeAndRunExample() {
     // ONNX model file name
     const onnxModelFileName = 'xgboost_AirQuality_ort.onnx';
 
-    // Load the ONNX model
-    const onnxModel = await ort.InferenceSession.create({ backendHint: 'webgl' });
-    await onnxModel.loadModel(`./${onnxModelFileName}`);
+    try {
+        // Fetch the ONNX model file as an array buffer
+        const response = await fetch(`./${onnxModelFileName}`);
+        const buffer = await response.arrayBuffer();
 
-    // Extract input and output names
-    const inputName = onnxModel.inputNames[0];
-    const outputName = onnxModel.outputNames[0];
+        // Load the ONNX model using the array buffer
+        const onnxModel = await ort.InferenceSession.create({ backendHint: 'webgl' });
+        await onnxModel.loadModel(buffer);
 
-    // Function to collect input values from text boxes
-    function collectInputValues() {
-        const inputValues = {};
+        // Extract input and output names
+        const inputName = onnxModel.inputNames[0];
+        const outputName = onnxModel.outputNames[0];
 
-        // Replace with your actual input ids
-        const inputIds = [
-            'box1', 'box2', 'box3', 'box4', 'box5', 'box6',
-            'box7', 'box8', 'box9', 'box10', 'box11', 'box12'
-        ];
+        // Function to collect input values from text boxes
+        function collectInputValues() {
+            const inputValues = {};
 
-        inputIds.forEach(id => {
-            const inputValue = document.getElementById(id).value;
-            inputValues[id] = inputValue ? parseFloat(inputValue) : 0;
-        });
+            // Replace with your actual input ids
+            const inputIds = [
+                'box1', 'box2', 'box3', 'box4', 'box5', 'box6',
+                'box7', 'box8', 'box9', 'box10', 'box11', 'box12'
+            ];
 
-        return inputValues;
+            inputIds.forEach(id => {
+                const inputValue = document.getElementById(id).value;
+                inputValues[id] = inputValue ? parseFloat(inputValue) : 0;
+            });
+
+            return inputValues;
+        }
+
+        // Function to update predictions when the button is clicked
+        window.runExample = async function () {
+            // Collect input values from text boxes
+            const inputValues = collectInputValues();
+
+            // Prepare the input data for ONNX model
+            const inputData = new Float32Array(Object.values(inputValues));
+
+            // Create an ONNX Tensor from the input data
+            const inputTensor = new ort.Tensor(ort.WebGLFloat32, new Float32Array(inputData), [1, Object.keys(inputValues).length]);
+
+            // Run the ONNX model to get predictions
+            const outputTensor = await onnxModel.run([outputName], { [inputName]: inputTensor });
+
+            // Get the prediction result
+            const predictionResult = outputTensor.getValues();
+
+            // Display the prediction result
+            document.getElementById('predictions').textContent = `Predicted AQI: ${predictionResult[0].toFixed(2)}`;
+        };
+
+        // Update the model status on the HTML page
+        document.getElementById('modelLoaded').textContent = 'Yes';
+    } catch (error) {
+        console.error('Error loading the ONNX model:', error);
+        document.getElementById('modelLoaded').textContent = 'No';
     }
-
-    // Function to update predictions when the button is clicked
-    window.runExample = async function () {
-        // Collect input values from text boxes
-        const inputValues = collectInputValues();
-
-        // Prepare the input data for ONNX model
-        const inputData = new Float32Array(Object.values(inputValues));
-
-        // Create an ONNX Tensor from the input data
-        const inputTensor = new ort.Tensor(ort.WebGLFloat32, new Float32Array(inputData), [1, Object.keys(inputValues).length]);
-
-        // Run the ONNX model to get predictions
-        const outputTensor = await onnxModel.run([outputName], { [inputName]: inputTensor });
-
-        // Get the prediction result
-        const predictionResult = outputTensor.getValues();
-
-        // Display the prediction result
-        document.getElementById('predictions').textContent = `Predicted AQI: ${predictionResult[0].toFixed(2)}`;
-    };
-
-    // Update the model status on the HTML page
-    document.getElementById('modelLoaded').textContent = 'Yes';
 }
 
 // Call the initialization function
