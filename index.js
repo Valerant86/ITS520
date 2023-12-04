2f979f4f33be539b6a77e9195e74c755a7a2b979
Valerant86 <144962202+Valerant86@users.noreply.github.com>
2023-12-03 21:28:13 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,35 @@
- async function runExample() {
-      // Fetch values from input boxes
-      var x = new Float32Array(12);
-      for (let i = 1; i <= 12; i++) {
-        x[i - 1] = parseFloat(document.getElementById(`box${i}`).value);
-      }
+async function runExample() {
+  // Fetch values from input boxes
+  var x = new Float32Array(12);
+  for (let i = 1; i <= 12; i++) {
+    x[i - 1] = parseFloat(document.getElementById(`box${i}`).value);
+  }
 
-      // Create the input tensor
-      let tensorX = new ort.Tensor('float32', x, [1, 12]);
-      let feeds = { float_input: tensorX };
+  // Create the input tensor
+  let tensorX = new ort.Tensor('float32', x, [1, 12]);
+  let feeds = { float_input: tensorX };
 
-      // Load the ONNX model
-      let session = await ort.InferenceSession.create('xgboost_AirQuality_ort.onnx');
+  // Load the ONNX model
+  let session = await ort.InferenceSession.create('xgboost_AirQuality_ort.onnx');
 
-      // Run the model and get the result
-      let result = await session.run(feeds);
-      let outputData = result.variable.data;
+  // Run the model and get the result
+  let result = await session.run(feeds);
+  let outputData = result.tensorMap.float_output.data; // Update this line to get the correct output
 
-      // Format the output value
-      outputData = parseFloat(outputData).toFixed(2);
+  // Format the output value
+  outputData = parseFloat(outputData[0]).toFixed(2);
 
-      // Display the output value
-      let predictions = document.getElementById('predictions');
-      predictions.innerHTML = ` <hr> Got an output tensor with values: <br/>
-       <table>
-         <tr>
-           <td>  Rating of Air Quality  </td>
-           <td id="td0">  ${outputData}  </td>
-         </tr>
-      </table>`;
+  // Display the output value
+  let predictions = document.getElementById('predictions');
+  predictions.innerHTML = ` <hr> Got an output tensor with values: <br/>
+   <table>
+     <tr>
+       <td>  Rating of Air Quality  </td>
+       <td id="td0">  ${outputData}  </td>
+     </tr>
+  </table>`;
 
-      // Change the background color based on the output value
-      var boxColor = getBoxColor(outputData);
-      predictions.style.backgroundColor = boxColor;
-    }
-
-    function getBoxColor(aqi) {
-
-      if (aqi <= 50) {
-        return "green"; // Good
-      } else if (aqi <= 100) {
-        return "yellow"; // Moderate
-      } else if (aqi <= 150) {
-        return "orange"; // Unhealthy for sensitive groups
-      } else if (aqi <= 200) {
-        return "red"; // Unhealthy
-      } else if (aqi <= 300) {
-        return "purple"; // Very Unhealthy
-      } else {
-        return "maroon"; // Hazardous
-      }
-    }
-  </script>
-</body>
-</html>
+  // Change the background color based on the output value
+  var boxColor = getBoxColor(outputData);
+  predictions.style.backgroundColor = boxColor;
+}
